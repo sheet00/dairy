7ad2a503b367d57933db42eed3d35a31e78fa836
sheet00 <punta@jk2.so-net.ne.jp>
2015-02-14 00:32:31 +0900

    [fix] #14 iframe系の埋め込み可変サイズ
    bootstrapで対応

--- app/helpers/diaries_helper.rb
+++ app/helpers/diaries_helper.rb
@@ -1,2 +1,23 @@
 module DiariesHelper
+
+  def convert_content(str)
+    str = br(str)
+    str = responsive_embed(str)
+    str = Sanitize.clean(str, Sanitize::Config::RELAXED_CUSTOM)
+    str.html_safe
+  end
+
+  private
+  def br(str)
+    (str).gsub(/(\r\n?)|(\n)/, "<br />")
+  end
+
+  def responsive_embed(str)
+    #<div class="embed-responsive embed-responsive-16by9">
+    doc = Nokogiri::HTML(str)
+    doc.css("iframe").wrap('<div class="embed-responsive embed-responsive-16by9">')
+
+    doc.to_html
+  end
+
 end
