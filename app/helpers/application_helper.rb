cec6e500d91a06f6c646304955b01507c71f0e61
sheet00 <punta@jk2.so-net.ne.jp>
2015-02-27 22:33:59 +0900

    [fix] #12 初回再生時の配列取得修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,36 +4,4 @@ module ApplicationHelper
       datetime.strftime("%Y/%m/%d %H:%M")
     end
   end
-
-  def last_fm_info
-    #sample
-    "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=26ea27392a474f50a2a25225cc6a963d&format=json"
-
-    api_key = Settings.last_fm[:api_key]
-    user = Settings.last_fm[:user]
-
-    unless api_key || user then return end
-
-    uri = URI.parse("http://ws.audioscrobbler.com/2.0/")
-    uri.query = {
-      :method => "user.getrecenttracks",
-      :user => user,
-      :api_key => api_key,
-      :limit => 1,
-      :format => :json
-    }.to_param
-
-    json = Net::HTTP.get(uri)
-    json_hash = JSON.parse(json)
-    json_hash = json_hash["recenttracks"]["track"].first
-
-    result = {}
-    result[:artist] = json_hash["artist"]["#text"]
-    result[:name] = json_hash["name"]
-    result[:album] = json_hash["album"]["#text"]
-    result[:url] = json_hash["url"]
-    result[:image] = json_hash["image"][2]["#text"]
-
-    result
-  end
 end
